b4a9d80b4d9ffc38b0f34e2365dec96365c22f6c
Alessandro Gario <alessandro.gario@gmail.com>
2020-05-22 03:14:21 +0200

    Update copyright headers

--- components/JSONConfiguration/src/JSONConfiguration.h
+++ components/JSONConfiguration/src/JSONConfiguration.h
@@ -1,10 +1,10 @@
 /*
-Copyright (c) 2019-present, Trail of Bits, Inc.
-All rights reserved.
+ Copyright (c) 2019-present, Trail of Bits, Inc.
+ All rights reserved.
 
-This source code is licensed in accordance with the terms specified in
-the LICENSE file found in the root directory of this source tree.
-*/
+ This source code is licensed in accordance with the terms specified in
+ the LICENSE file found in the root directory of this source tree.
+ */
 
 #import <Foundation/Foundation.h>
 
--- components/NotificationService/src/NotificationService.h
+++ components/NotificationService/src/NotificationService.h
@@ -1,10 +1,10 @@
-//
-//  NotificationService.h
-//  NotificationService
-//
-//  Created by Alessandro on 16/04/2020.
-//  Copyright © 2020 Trail of Bits. All rights reserved.
-//
+/*
+ Copyright (c) 2019-present, Trail of Bits, Inc.
+ All rights reserved.
+
+ This source code is licensed in accordance with the terms specified in
+ the LICENSE file found in the root directory of this source tree.
+ */
 
 #import <Foundation/Foundation.h>
 
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double NotificationServiceVersionNumber;
 
 //! Project version string for NotificationService.
 FOUNDATION_EXPORT const unsigned char NotificationServiceVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <NotificationService/PublicHeader.h>
-
-
